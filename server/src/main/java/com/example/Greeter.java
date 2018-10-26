42e40af27f0d7275e9aacc6fb03d37f8df0d74d5
Brett J Levene <brettjlevene@gmail.com>
2018-10-26 20:28:20 +0100

    trying this out too.

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,10 +13,10 @@ public class Greeter {
   }
 
   /**
-   * @param someone
-   * @return somewhere
+   * @param someone the name of the person
+   * @return greeting string
    */
-  public final String greet(final String @someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
